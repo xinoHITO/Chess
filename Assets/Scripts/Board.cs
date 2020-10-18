63fa177d9676cf760d94a19242962718abaf2457
Hitoshi Kanno <hkannok@gmail.com>
2020-10-18 09:23:04 -0500

    Board and chess piece foundation logic.

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public GridSpace GridSpacePrefab;
+    public float GridSpaceSize = 1;
+
+    [Header("Board size")]
+    public int Rows = 8;
+    public int Columns = 8;
+
+    public static Board Instance;
+
+    private static GridSpace[] GridSpaces;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else {
+            Destroy(this);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateBoardSpaces();
+
+        CacheBoardSpaces();
+    }
+
+    private void CreateBoardSpaces()
+    {
+        for (int i = 0; i < Rows; i++)
+        {
+            for (int j = 0; j < Columns; j++)
+            {
+                GridSpace boardGrid = Instantiate(GridSpacePrefab, transform);
+                boardGrid.x = j;
+                boardGrid.y = i;
+                boardGrid.transform.position = transform.position + new Vector3(GridSpaceSize * j, 0, GridSpaceSize * i);
+            }
+        }
+    }
+
+    private void CacheBoardSpaces()
+    {
+        if (GridSpaces == null)
+        {
+            GridSpaces = FindObjectsOfType<GridSpace>();
+        }
+    }
+
+    public GridSpace GetGridSpace(int x, int y)
+    {
+        foreach (var gridSpace in GridSpaces)
+        {
+            if (gridSpace.x == x && gridSpace.y == y)
+            {
+                return gridSpace;
+            }
+        }
+        return null;
+    }
+
+}
+
---
+++ Assets/Scripts/ChessPiece.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChessPiece : MonoBehaviour
+{
+    private static Board BoardManager;
+
+    void Start()
+    {
+        BoardManager = Board.Instance;
+        if (BoardManager == null)
+        {
+            Debug.LogError("No board found");
+            return;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            MoveTo(UnityEngine.Random.Range(0, 10), UnityEngine.Random.Range(0, 10));
+        }
+    }
+
+    public void MoveTo(int x, int y)
+    {
+        if (x >= BoardManager.Rows || y >= BoardManager.Columns)
+        {
+            Debug.LogError(string.Format("Invalid board position x:{0} | y:{1}",x,y));
+            return;
+        }
+
+        var gridSpace = BoardManager.GetGridSpace(x, y);
+        if (gridSpace == null)
+        {
+            Debug.LogError("Board space not found");
+            return;
+        }
+
+        transform.position = gridSpace.transform.position;
+    }
+
+    
+}
